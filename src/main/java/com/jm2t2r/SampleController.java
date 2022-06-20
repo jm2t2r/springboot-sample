69645d2d7ef1d8977d1c1d0dc8d62c557c8b51a7
jm2t2r <87157866+jm2t2r@users.noreply.github.com>
2022-06-20 10:27:54 +0900

    Update SampleController.java

--- src/main/java/com/jm2t2r/SampleController.java
+++ src/main/java/com/jm2t2r/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,8 +26,30 @@ public class SampleController {
         logger.info("info log");
         logger.debug("debug log");
         logger.trace("trace log");
-
+ 
         logger.info(result);
+        
+        
         return result;
+    }
+    @RequestMapping("/sleep")
+    public String sleep(HttpServletRequest request){
+    	String result="";
+    	
+    	long seed = System.currentTimeMillis();
+    	Random rand = new Random(seed);
+    	long lValue = rand.nextInt(5000); // 0 <= iValue < 5000
+    	
+    	try {
+			Thread.sleep(lValue);
+			result = "this thread sleep for "+lValue+"ms";
+			logger.debug(result);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    
+    	return result;
+    
     }
 }
